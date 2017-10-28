1dc7866d57c45f39e1a4784fce0839b94990f683
Helen Liutongco <bellpickle@gmail.com>
2017-10-28 06:54:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ function takeANumber(katzDeliLine,name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
+
+function nowServing(katzDeliLine) {
+  if(katzDeliLine>[]) {
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }
+  else {
+    return "There is nobody waiting to be served!"
+  }
+}
